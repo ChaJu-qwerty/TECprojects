5fe002527bfea991d38a5aeaa34e11910f27c6d2
ChaJu-qwerty <108501861+ChaJu-qwerty@users.noreply.github.com>
2023-10-10 07:25:52 -0700

    Add files via upload

---
+++ PROYECTO_INTEGRADOR_COMPU/PROYECTO INT PROGRA/CalculadoraUF.py
@@ -0,0 +1,11 @@
+from tkinter import Tk
+#el def menu debe e tener la pagina de inicio
+#def menu():
+
+
+raiz=Tk()
+raiz.title("Calculadora de Dinamica (Fisica)") #titulo a la ventana
+raiz.iconbitmap("iconoATM.ico")
+raiz.resizable(0,0) #ancho,alto, 0,0 = no se puede cambiar tamaño= Fañse,False
+#raiz.
+raiz.mainloop() # la interfaz que debe quedarse esperando a que el usuario haga algo
