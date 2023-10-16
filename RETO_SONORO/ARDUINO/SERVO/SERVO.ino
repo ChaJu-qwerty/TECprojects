70db9a93768a5100a2f5eebd3b2d38fa11e0a39e
ChaJu-qwerty <108501861+ChaJu-qwerty@users.noreply.github.com>
2023-10-15 18:54:22 -0700

    Update SERVO.ino

--- RETO_SONORO/ARDUINO/SERVO/SERVO.ino
+++ RETO_SONORO/ARDUINO/SERVO/SERVO.ino
@@ -1,23 +1,21 @@
-#include <Servo.h>
+  // put your setup code here, to run once:
+servo1.attach(Pinservo1);
+servo2.attach(Pinservo2);
+servo3.attach(Pinservo3);
 
-#include <Servo.h>
-
-int PinServo = 9;
-int anguloServo = 90;
-Servo miMotor;
-#include <Servo.h>
-// C++ code
-//
-void setup()
-{
-  miMotor.attach(PinServo);
-  Serial.begin(9600);
+//como estaran al inicio
+servo1.write(0,150,true); //angulo, velocidad, va a esperar a que este en 0 para la siguiente linea
+servo2.write(0,150,true);
 }
 
-void loop()
-{
-  miMotor.write(anguloServo);
-  delay(2000);
-  miMotor.write(180);
-  delay(2000);
+void loop() {
+  // put your main code here, to run repeatedly:
+servo1.write(90,150,true);
+servo1.write(0,100,true);
+servo2.write(90,150,true);
+servo2.write(0,150,false);
+servo3.write(90,150,true);
+servo3.write(0,150,false);
+delay(3000);
+
 }
